46c65a6df97b1165d790bdb77db4ac37f89f56fb
José Luiz <luizbsilva@gmail.com>
2024-07-07 14:30:28 -0300

    feature: add userRepository

---
+++ user-service-api/src/main/java/br/com/kayros/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package br.com.kayros.repository;
+
+import br.com.kayros.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByEmail(final String email);
+
+    void deleteByEmail(final String validEmail);
+}
